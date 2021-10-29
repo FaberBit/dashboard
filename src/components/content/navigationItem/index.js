2a6a831d000136055a0bff738a8c89e22f535e06
DevTheofanes <devtheofanes@gmail.com>
2021-10-29 17:19:40 -0300

    home page

--- src/components/content/navigationItem/index.js
+++ src/components/content/navigationItem/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { ItemContainer } from './styles';
 
-import { useUser } from '../../../hooks/useUser'
-
 export function NavigationItem({ title, subTitle, source, path }) {
-  const { host } = useUser()
   return (
     <ItemContainer to={path}>
       <h4>{title}</h4>
--- src/pages/private/home/index.js
+++ src/pages/private/home/index.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import { HeaderMacro } from '../../../components/headers/macroComponent'
 import { NavigationItem } from '../../../components/content/navigationItem';
-// import { HeaderMacro } from '../../../components/headerMacro';
-import {HeaderMacro} from '../../../components/headers/macroComponent'
-// import { IoLogInOutline } from "react-icons/io5";
 
-import BannerLogin from '../../../assets/img/banners/bannerLoginA.png'
-// import LogoOfficial from '../../../assets/img/logos/official.png'
 
-// import { Input } from '../../../components/inputs/simples';
 import { Container, Content } from './styles';
+
+import DataImg from '../../../assets/img/banners/data.svg'
+import NewImg from '../../../assets/img/banners/new.svg'
+import NotifyImg from '../../../assets/img/banners/notify.svg'
+import ProfileImg from '../../../assets/img/banners/profile.svg'
+import FeatureImg from '../../../assets/img/banners/feature.svg'
+import BugImg from '../../../assets/img/banners/bug.svg'
 
 export function Home() {
   return (
@@ -16,17 +19,17 @@ export function Home() {
       <HeaderMacro title="Administre sua empresa facilmente."/>
 
       <Content>
-        <NavigationItem title="Visualize seus dados" subTitle="em tempo real" path="/find/view" source={BannerLogin}/>
+        <NavigationItem title="Visualize seus dados" subTitle="em tempo real" path="/find/view" source={DataImg}/>
         
-        <NavigationItem title="Cadastre novos dados" subTitle="com poucos clicks" path="/find/view" source={BannerLogin}/>
+        <NavigationItem title="Cadastre novos dados" subTitle="com poucos clicks" path="/find/view" source={NewImg}/>
         
-        <NavigationItem title="Acesse suas notificações" subTitle="para ficar informado" path="/find/view" source={BannerLogin}/>
+        <NavigationItem title="Acesse suas notificações" subTitle="para ficar informado" path="/find/view" source={NotifyImg}/>
         
-        <NavigationItem title="Atualize sua conta" subTitle="com facilidade" path="/find/view" source={BannerLogin}/>
+        <NavigationItem title="Atualize sua conta" subTitle="com facilidade" path="/find/view" source={ProfileImg}/>
         
-        <NavigationItem title="Novas funcionalidades" subTitle="em desenvolvimento" path="/find/view" source={BannerLogin}/>
+        <NavigationItem title="Novas funcionalidades" subTitle="em desenvolvimento" path="/find/view" source={FeatureImg}/>
         
-        <NavigationItem title="Reporte algum bug" subTitle="e iremos corrigir" path="/find/view" source={BannerLogin}/>
+        <NavigationItem title="Reporte algum bug" subTitle="e iremos corrigir" path="/find/view" source={BugImg}/>
       </Content>
 
 
