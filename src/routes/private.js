b0bf2cdd922a031807b97ef50cd1e59d14947cb6
DevTheofanes <devtheofanes@gmail.com>
2021-11-23 11:41:54 -0300

    feat: new page ViewInfos

--- src/routes/private.js
+++ src/routes/private.js
@@ -4,12 +4,14 @@ import { FindView } from '../pages/private/find/view';
 // import { useUser } from '../hooks/useUser';
 
 import { Home } from '../pages/private/home';
+import { View } from '../pages/private/view';
 
 export function PrivateRoutes() {
   return (
     <Switch>
       <Route path="/" exact component={Home}/>
       <Route path="/find/view" exact component={FindView}/>
+      <Route path="/view/:title/:encounter" exact component={View}/>
     </Switch>
   );
 }
