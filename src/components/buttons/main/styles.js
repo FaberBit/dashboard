37da19b9b8b05cb95b67c2858224bb6b21829459
DevTheofanes <devtheofanes@gmail.com>
2021-11-25 18:15:28 -0300

    refactor: styles from button container

--- src/components/buttons/main/styles.js
+++ src/components/buttons/main/styles.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.button`
   span {
+    font-style: normal;
     font-weight: 600;
     font-size: 14px;
     line-height: 21px;
 
-    color: #ffffff;
+    color: #FFFFFF;
   }
 
   width: ${(props) => (props.width ? `${String(props.width)}px` : "416px")};
