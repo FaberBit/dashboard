ff9d891299a21cb37b326c4819627ad138a8c7c9
DevTheofanes <devtheofanes@gmail.com>
2021-11-30 20:24:49 -0300

    fix: new boatDetails page

---
+++ src/pages/private/entity/boat/details/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { HeaderMain } from '../../../../../components/headers/main'
+
+
+import { Container, Content} from './styles';
+
+export function BoatDetails() {
+  return (
+    <Container>
+      <HeaderMain title="Lanchas"/>
+
+      <Content>
+        <h4>Dados</h4>
+      </Content>
+    </Container>
+  )
+}
+
---
+++ src/pages/private/entity/boat/details/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 2.5rem 3.75rem;
+  height: 100vh;
+  background-color: #f8f9fb;
+`;
+
+export const Content = styled.div`
+  display: flex;
+`;
