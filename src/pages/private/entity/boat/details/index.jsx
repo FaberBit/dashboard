e32a5fd6e11df0da0e7093618292204c50c958f5
DevTheofanes <devtheofanes@gmail.com>
2021-11-30 21:21:08 -0300

    refactor: styles boatDetails page

--- src/pages/private/entity/boat/details/index.jsx
+++ src/pages/private/entity/boat/details/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import { FiEdit2, FiArrowLeft, FiArrowRight } from "react-icons/fi";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 import { HeaderMain } from '../../../../../components/headers/main'
-
-
-import { Container, Content} from './styles';
+import { ButtonsPages, Container, Content, HeaderContent, ItemInfo } from './styles';
 
 export function BoatDetails() {
   return (
@@ -11,7 +11,122 @@ export function BoatDetails() {
       <HeaderMain title="Lanchas"/>
 
       <Content>
-        <h4>Dados</h4>
+        <HeaderContent>
+            <strong>Nome da embarcação</strong>
+            <strong>Proprietário</strong>
+            <strong>Marca</strong>
+            <strong>Modelo</strong>
+            <strong>Data de criação</strong>
+            <strong>Opções</strong>
+        </HeaderContent>
+
+        <ItemInfo>
+            <strong>Perolá Negra dos Sete Mares Escuros</strong>
+            <strong>João Carlos</strong>
+            <strong>Ford</strong>
+            <strong>GTX1299</strong>
+            <strong>12/08/2003</strong>
+            <div className="actionsButton">
+                <a href="/boat/edit">
+                    <FiEdit2 color="#000" size={22}/>
+                </a>
+                <button>
+                    <RiDeleteBin6Line color="#000" size={22}/>
+                </button>
+            </div>
+        </ItemInfo>
+
+        <ItemInfo>
+            <strong>Perolá Negra dos Sete Mares Escuros</strong>
+            <strong>João Carlos</strong>
+            <strong>Ford</strong>
+            <strong>GTX1299</strong>
+            <strong>12/08/2003</strong>
+            <div className="actionsButton">
+                <a href="/boat/edit">
+                    <FiEdit2 color="#000" size={22}/>
+                </a>
+                <button>
+                    <RiDeleteBin6Line color="#000" size={22}/>
+                </button>
+            </div>
+        </ItemInfo>
+
+        <ItemInfo>
+            <strong>Perolá Negra dos Sete Mares Escuros</strong>
+            <strong>João Carlos</strong>
+            <strong>Ford</strong>
+            <strong>GTX1299</strong>
+            <strong>12/08/2003</strong>
+            <div className="actionsButton">
+                <a href="/boat/edit">
+                    <FiEdit2 color="#000" size={22}/>
+                </a>
+                <button>
+                    <RiDeleteBin6Line color="#000" size={22}/>
+                </button>
+            </div>
+        </ItemInfo>
+
+        <ItemInfo>
+            <strong>Perolá Negra dos Sete Mares Escuros</strong>
+            <strong>João Carlos</strong>
+            <strong>Ford</strong>
+            <strong>GTX1299</strong>
+            <strong>12/08/2003</strong>
+            <div className="actionsButton">
+                <a href="/boat/edit">
+                    <FiEdit2 color="#000" size={22}/>
+                </a>
+                <button>
+                    <RiDeleteBin6Line color="#000" size={22}/>
+                </button>
+            </div>
+        </ItemInfo>
+
+        <ItemInfo>
+            <strong>Perolá Negra dos Sete Mares Escuros</strong>
+            <strong>João Carlos</strong>
+            <strong>Ford</strong>
+            <strong>GTX1299</strong>
+            <strong>12/08/2003</strong>
+            <div className="actionsButton">
+                <a href="/boat/edit">
+                    <FiEdit2 color="#000" size={22}/>
+                </a>
+                <button>
+                    <RiDeleteBin6Line color="#000" size={22}/>
+                </button>
+            </div>
+        </ItemInfo>
+
+        <ItemInfo>
+            <strong>Perolá Negra dos Sete Mares Escuros</strong>
+            <strong>João Carlos</strong>
+            <strong>Ford</strong>
+            <strong>GTX1299</strong>
+            <strong>12/08/2003</strong>
+            <div className="actionsButton">
+                <a href="/boat/edit">
+                    <FiEdit2 color="#000" size={22}/>
+                </a>
+                <button>
+                    <RiDeleteBin6Line color="#000" size={22}/>
+                </button>
+            </div>
+        </ItemInfo>
+
+        <ButtonsPages>
+            <button>
+                <FiArrowLeft size={20}/>
+            </button>
+
+            <span>01</span>
+
+            <button>
+                <FiArrowRight size={20}/>
+            </button>
+        </ButtonsPages>
       </Content>
     </Container>
   )
