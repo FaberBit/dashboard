026ac17a81b187abcb2371f438d9b12333267fde
DevTheofanes <devtheofanes@gmail.com>
2021-12-04 21:21:21 -0300

    feat: new entityEmployee pages

---
+++ src/pages/private/entity/employee/new/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { HeaderMain } from '../../../../../components/headers/main'
+
+
+import { Container, Content, Form } from './styles';
+
+import NewSvg from '../../../../../assets/img/banners/new.svg'
+import { Input } from '../../../../../components/inputs/main';
+import { Button } from '../../../../../components/buttons/main';
+
+export function EmployeeNew() {
+  return (
+    <Container>
+      <HeaderMain title="Crie um Funcionario"/>
+
+      <Content>
+        <img src={NewSvg} alt='Form' />
+
+        <Form>
+          <h4>Preencha os campos</h4>
+
+          <Input title="Nome" width={316} placeholder="Nome do cliente"/>
+
+          <Input title="Email" width={316} placeholder="E-mail mais utilizado pelo cliente"/>
+
+          <Input title="Telefone" width={316} placeholder="Telefone para contato do cliente"/>
+
+          <Button title="Salvar" width={140}/>
+        </Form>
+      </Content>
+    </Container>
+  )
+}
+
---
+++ src/pages/private/entity/employee/update/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { HeaderMain } from '../../../../../components/headers/main'
+
+
+import { Container, Content, Form } from './styles';
+
+import NewSvg from '../../../../../assets/img/banners/new.svg'
+import { Input } from '../../../../../components/inputs/main';
+import { Button } from '../../../../../components/buttons/main';
+
+export function EmployeeEdit() {
+  return (
+    <Container>
+      <HeaderMain title="Edite o Funcionario"/>
+
+      <Content>
+        <img src={NewSvg} alt='Form' />
+
+        <Form>
+          <h4>Revise os campos</h4>
+
+          <Input title="Nome" width={316} placeholder="Nome" value="any name"/>
+
+          <Input title="Email" width={316} placeholder="Email" value="any@mail.com"/>
+
+          <Input title="Telefone" width={316} placeholder="Telefone" value="any_telefone"/>
+
+          <Button title="Salvar" width={140}/>
+        </Form>
+      </Content>
+    </Container>
+  )
+}
+
+
