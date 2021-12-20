96d1ec472ab48afd3e15035a6be5c83359b8be85
DevTheofanes <devtheofanes@gmail.com>
2021-12-20 17:48:13 -0300

    feat: new page ReviewsNew

---
+++ src/pages/private/entity/reviews/new/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { HeaderMain } from '../../../../../components/headers/main'
+
+
+import { Container, Content, Form } from './styles';
+
+import NewSvg from '../../../../../assets/img/banners/new.svg'
+import { Input } from '../../../../../components/inputs/main';
+import { Button } from '../../../../../components/buttons/main';
+
+export function ReviewsNew() {
+  return (
+    <Container>
+      <HeaderMain title="Crie uma nova Revisão"/>
+
+      <Content>
+        <img src={NewSvg} alt='Form' />
+
+        <Form>
+          <h4>Preencha os campos</h4>
+
+          <Input title="Embarcação" width={416} placeholder="Embacação do cliente"/>
+
+          <Input title="Empresa" width={416} placeholder="Empresa que realizou a revisão"/>
+
+          <Input title="Profissional" width={416} placeholder="Profissional que realizou a revisão"/>
+
+          <Input title="Hora/Motor" width={416} placeholder="Hora/Motor de quando a revisão foi realizada"/>
+
+          <Input title="Proxima Revisão p/ Mês" width={416} placeholder="Data da proxima revisão pelo mês"/>
+
+          <Input title="Proxima Revisão p/ Hora/Motor " width={416} placeholder="Data da proxima revisão por quilometragem"/>
+
+          <Button title="Salvar" width={140}/>
+        </Form>
+      </Content>
+    </Container>
+  )
+}
+
