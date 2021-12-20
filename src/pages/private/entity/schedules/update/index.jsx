1aa52aff85f65d2e5d3f3e7c9bb8728815dfaa72
DevTheofanes <devtheofanes@gmail.com>
2021-12-20 17:32:06 -0300

    feat: new page SchedulesUpdate

---
+++ src/pages/private/entity/schedules/update/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { HeaderMain } from '../../../../../components/headers/main'
+
+
+import { Container, Content, Form } from './styles';
+
+import NewSvg from '../../../../../assets/img/banners/new.svg'
+import { Input } from '../../../../../components/inputs/main';
+import { Button } from '../../../../../components/buttons/main';
+
+export function SchedulesEdit() {
+  return (
+    <Container>
+      <HeaderMain title="Edite Jetski"/>
+
+      <Content>
+        <img src={NewSvg} alt='Form' />
+
+        <Form>
+          <h4>Revise os campos</h4>
+          <Input title="Proprietário" width={416} placeholder="Dono da embarcação" value="Any_Name"/>
+
+          <Input title="Embarcação" width={416} placeholder="Embacação do cliente" value="Any_Name"/>
+
+          <Input title="Descrição" width={416} placeholder="Observações do agendamento" value="Any_Description"/>
+
+          <Input title="Data" width={416} placeholder="Dia do agendamento" value="Any_Day"/>
+
+          <Input title="Hora" width={416} placeholder="Horario do agendamento" value="Any_Hour"/>
+
+          <Button title="Salvar" width={140}/>
+        </Form>
+      </Content>
+    </Container>
+  )
+}
