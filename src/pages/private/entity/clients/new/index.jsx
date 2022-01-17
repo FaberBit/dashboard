642441dcf7aff25f7a855e48d8475e2df4acd038
DevTheofanes <devtheofanes@gmail.com>
2022-01-17 18:42:40 -0300

    feat: add connection client to server

--- src/pages/private/entity/clients/new/index.jsx
+++ src/pages/private/entity/clients/new/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { toast } from 'react-toastify';
 import { HeaderMain } from '../../../../../components/headers/main';
 
 import { Container, Content, Form } from './styles';
@@ -8,7 +9,32 @@ import NewSvg from '../../../../../assets/img/banners/new.svg';
 import { Input } from '../../../../../components/inputs/main';
 import { Button } from '../../../../../components/buttons/main';
 
+import api from '../../../../../services/api';
+import { history } from '../../../../../services/history';
+
 export function ClientNew() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+
+  async function handleSaveData() {
+    try {
+      const response = await api.post('/users', {
+        name,
+        email,
+        password,
+        phone,
+        employee: false,
+      });
+      history.push('/client/infos');
+      toast.success(`${response.data.name} agora é um cliente da Enos.`);
+    } catch (error) {
+      console.log(error.response);
+      toast.error(`${error.response.data.error}`);
+    }
+  }
+
   return (
     <Container>
       <HeaderMain title="Crie um Cliente" />
@@ -19,13 +45,39 @@ export function ClientNew() {
         <Form>
           <h4>Preencha os campos</h4>
 
-          <Input title="Nome" width={316} placeholder="Nome do cliente" />
+          <Input
+            title="Nome"
+            width={316}
+            placeholder="Nome do cliente"
+            value={name}
+            onChange={(e) => { setName(e.target.value); }}
+          />
 
-          <Input title="Email" width={316} placeholder="E-mail mais utilizado pelo cliente" />
+          <Input
+            title="Email"
+            width={316}
+            placeholder="E-mail mais utilizado pelo cliente"
+            value={email}
+            onChange={(e) => { setEmail(e.target.value); }}
+          />
 
-          <Input title="Telefone" width={316} placeholder="Telefone para contato do cliente" />
+          <Input
+            title="Senha"
+            width={316}
+            placeholder="Uma senha segura para o cliente"
+            value={password}
+            onChange={(e) => { setPassword(e.target.value); }}
+          />
 
-          <Button title="Salvar" width={140} />
+          <Input
+            title="Telefone"
+            width={316}
+            placeholder="Telefone para contato do cliente"
+            value={phone}
+            onChange={(e) => { setPhone(e.target.value); }}
+          />
+
+          <Button title="Salvar" width={140} onClick={() => handleSaveData()} />
         </Form>
       </Content>
     </Container>
