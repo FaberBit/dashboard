c8cfc4f043462d4f96285f629dbf36796c7c173a
DevTheofanes <devtheofanes@gmail.com>
2022-01-18 11:47:51 -0300

    feat: new Component to Select

---
+++ src/components/inputs/select/index.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Container } from './styles';
+
+export function Select({
+  title,
+  width,
+  stylesProps,
+  messageFirst,
+  data,
+  clientSelected,
+  setClientSelected,
+  // ...rest
+}) {
+  return (
+    <Container stylesProps={stylesProps} width={width}>
+      <span>{title}</span>
+      <select
+        onChange={(e) => {
+          setClientSelected(e.target.value);
+        }}
+        value={clientSelected}
+        // {...rest}
+      >
+        <option value="0">{messageFirst}</option>
+        {
+          data.map((i) => (
+            // <option key={i.id} value={String(i.id)}>{i.name}</option>
+            <option key={i.id} value={JSON.stringify(i)}>{i.name}</option>
+          ))
+        }
+      </select>
+    </Container>
+  );
+}
